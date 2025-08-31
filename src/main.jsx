7fd6b688b3bfac5afb9c3b09b6399bcd69eb3918
akshay0dkd <akshaybhiogade55@gmail.com>
2025-08-31 17:48:32 +0530

    Fixed finally

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/MovieFlex' >
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
